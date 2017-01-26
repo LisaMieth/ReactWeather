bccea4dc9c1aff65c92a09f8466bb20b009250b4
LisaMieth <lisahafizovic@gmail.com>
2017-01-26 16:33:17 +1100

    Style and update examples

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,20 @@
 var React = require('React');
+var {Link} = require('react-router');
+
 
 var Examples = (props) => {
     return(
         <div>
-         <h3>Examples</h3>
-         <p>Welcome to examples page!</p>
+         <h1 className="text-center">Examples</h1>
+         <p>Here are a few example locations to try out</p>
+         <ol>
+          <li>
+           <Link to='/?location=Sydney'>Sydney, SYD</Link>   
+          </li>
+          <li>
+            <Link to='/?locaiton=London'>London</Link>  
+          </li>      
+         </ol>    
         </div>
     )
 };
